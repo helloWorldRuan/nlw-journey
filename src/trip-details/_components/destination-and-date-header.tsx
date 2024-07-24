2a8c3afb9e356aeb1662fff4002ab4e241072511
helloWorldRuan <ruan.rodrigues@sptech.school>
2024-07-24 13:20:50 -0300

    [feat]: create DestinationAndDateHeader component

---
+++ src/trip-details/_components/destination-and-date-header.tsx
@@ -0,0 +1,27 @@
+import { Calendar, MapPin, Settings2 } from 'lucide-react';
+import { Button } from '../../components/button';
+
+export function DestinationAndDateHeader() {
+	return (
+		<div className="bg-zinc-900 h-16 flex items-center justify-between px-4 shadow-shape rounded-xl">
+			<div className="flex items-center gap-2">
+				<MapPin className="size-5 text-400" />
+				<span className="text-zinc-100">Florianóplis, Brasil</span>
+			</div>
+
+			<div className="flex items-center gap-5">
+				<div className="flex items-center gap-2">
+					<Calendar className="size-5 text-400" />
+					<span className="text-zinc-100">17 a 23 de agosto</span>
+				</div>
+
+				<hr className="w-px h-6 bg-zinc-800" />
+
+				<Button variant="secondary">
+					Alterar local/data
+					<Settings2 className="size-5" />
+				</Button>
+			</div>
+		</div>
+	);
+}
